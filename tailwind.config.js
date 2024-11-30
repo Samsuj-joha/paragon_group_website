c3f96f1236d2c70b288db92c01ba10ea74c05584
Samsujjoha <samsujjoha99@gmail.com>
2024-11-30 13:06:01 +0600

    create all dropdowm pages in company and buisness activity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+    // './app/**/*.{js,ts,jsx,tsx}',  // Ensure app folder is included
+    // './pages/**/*.{js,ts,jsx,tsx}', // Ensure pages folder is included
+    // './components/**/*.{js,ts,jsx,tsx}', // Ensure components folder is included
+    // './layout/**/*.{js,ts,jsx,tsx}', // Ensure layout folder is included
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',  // Ensure app folder is included
@@ -7,7 +21,16 @@ module.exports = {
     './layout/**/*.{js,ts,jsx,tsx}', // Ensure layout folder is included
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '640px',  // Small screens and above
+        md: '768px',  // Medium screens and above
+        lg: '1024px', // Large screens and above
+        xl: '1280px', // Extra large screens and above
+        '2xl': '1536px', // 2x Extra large screens and above
+      },
+    },
   },
   plugins: [],
-};
+}
+
